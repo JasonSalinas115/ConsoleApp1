77b75831dde303c34bfe100a813c2f70dce9bd25
JasonSalinas115 <jasonsalinas2@my.whccd.edu>
2025-01-29 20:03:19 -0800

    Added for loops

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,38 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the day of the week (1-7): ");
-            string day = Console.ReadLine();
 
-            switch (day) 
+            for (int i = 0; i < 10; i++)
             {
-                case "1":
-                    Console.WriteLine("Monday");
-                    break;
-                case "2":
-                    Console.WriteLine("Tuesday");
-                    ; break;
-                case "3":
-                    Console.WriteLine("Wednesday");
-                    break;
-                case "4":
-                    Console.WriteLine("Thursday");
-                    break;
-                case "5":
-                    Console.WriteLine("Friday");
-                    break;
-                case "6":
-                    Console.WriteLine("Saturday");
-                    Console.WriteLine("Its the weekend");
-                    break;
-                case "7":
-                    Console.WriteLine("Sunday");
-                    break;
-                default:
-                    Console.WriteLine("Invaild Input");
-                    break;
+                Console.WriteLine(i);
             }
 
+            Console.Write("\n");
+
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Write("\n");
+
+            string output = "#";
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(output);
+                output += "#";
+            }
         }
     }
 }
