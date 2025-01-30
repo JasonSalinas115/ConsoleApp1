d7cce366b87b70101f05df58fcf3a50d53a440a9
JasonSalinas115 <jasonsalinas2@my.whccd.edu>
2025-01-29 19:08:53 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter any whole number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 0)
+            {
+                Console.WriteLine("The number you entered is zero");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("I don't like evaluating negative numbers.");
+            }
+            else if (num % 2 == 0)
+            {
+                Console.WriteLine("The number you entered is Even");
+            }
+            else 
+            {
+                Console.WriteLine("The number you entered is Odd");
+            }
+
+
+           
+
+
+
+            
+
+
+
+            
+           
+            
+        }
+    }
+}
